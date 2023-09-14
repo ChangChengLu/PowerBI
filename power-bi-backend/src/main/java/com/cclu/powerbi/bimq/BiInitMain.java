4440cddd5e462d4533b0e476bb840d5b0457b87f
ChangCheng Lu <15252342585@163.com>
2023-09-14 09:09:52 +0800

    fix

--- power-bi-backend/src/main/java/com/cclu/powerbi/bimq/BiInitMain.java
+++ power-bi-backend/src/main/java/com/cclu/powerbi/bimq/BiInitMain.java
@@ -13,18 +13,14 @@ import java.util.concurrent.TimeoutException;
  */
 public class BiInitMain {
 
-    private static final String EXCHANGE_NAME = "code_exchange";
-
-    private static final String QUEUE_NAME = "code_queue";
-
     public static void main(String[] args) {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
-            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "my_routingKey");
+            channel.exchangeDeclare(BiMqConstant.BI_EXCHANGE_NAME, "direct");
+            channel.queueDeclare(BiMqConstant.BI_QUEUE_NAME, true, false, false, null);
+            channel.queueBind(BiMqConstant.BI_QUEUE_NAME, BiMqConstant.BI_EXCHANGE_NAME, BiMqConstant.BI_ROUTING_KEY);
         } catch (IOException | TimeoutException e) {
             e.printStackTrace();
         }
